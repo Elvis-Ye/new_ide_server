d89d2038bddd91ea439a1bcf61d448052e145f20
elvis <rojohe@163.com>
2018-03-01 03:05:29 +0800

    Test

---
+++ src/main/java/com/ide/demo/new_ide_server/App.java
@@ -0,0 +1,13 @@
+package com.ide.demo.new_ide_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
